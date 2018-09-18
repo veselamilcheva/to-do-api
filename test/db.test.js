2d86d5eaff8f5201dba6369d18defd7bdc36a143
Vesela Milcheva <milchevavesela@gmail.com>
2018-09-17 22:55:34 -0400

    prettify tests description

--- test/db.test.js
+++ test/db.test.js
@@ -5,41 +5,43 @@ afterAll(() => {
     db.end()
 })
 
-it('postTodo creates data', async () => {
-    const data = await db.postTodo({
-        title: 'a',
-        description: 'b'
-    })
-    expect(data).toHaveProperty('insertId');
-    insertId = data.insertId;
-    expect(insertId).toBeTruthy();
-});
+describe('db for to dos', () => {
 
-it('getTodos returns data', async () => {
-    const data = await db.getTodos();
-    expect(data[0].id).toBeDefined(); //for advanced toMatchSchema
-    expect(data[0].title).toBeDefined();
-    expect(data[0].description).toBeDefined();
-    expect(data.length).toBeTruthy();
-});
+    it('creates to dos', async () => {
+        const data = await db.postTodo({
+            title: 'a',
+            description: 'b'
+        })
+        expect(data).toHaveProperty('insertId');
+        insertId = data.insertId;
+        expect(insertId).toBeTruthy();
+    });
+    
+    it('returns bulk to dos', async () => {
+        const data = await db.getTodos();
+        expect(data[0].id).toBeDefined(); //for advanced toMatchSchema
+        expect(data[0].title).toBeDefined();
+        expect(data[0].description).toBeDefined();
+        expect(data.length).toBeTruthy();
+    });
+    
+    it('updates to do', async () => {
+        const todo = {
+            id: insertId,
+            title: 'a',
+            description: 'b'
+        }
+        const data = await db.putTodo(todo);
+        expect(data.affectedRows).toEqual(1); // changed one row for sure
+        expect(data).toBeTruthy();
+    });
+    
+    it('deletes to do', async () => {
+        const data = await db.deleteTodo(insertId);
+        expect(data.affectedRows).toEqual(1);
+        expect(data).toBeTruthy();
+        const checkId = await db.getTodo(insertId);
+        expect(checkId.length).toEqual(0);
+    });
 
-it('update to do', async () => {
-    const todo = {
-        id: insertId,
-        title: 'a',
-        description: 'b'
-    }
-    const data = await db.putTodo(todo);
-    expect(data.affectedRows).toEqual(1); // changed one row for sure
-    expect(data).toBeTruthy();
-});
-
-it('deleteTodo deletes data', async () => {
-    const data = await db.deleteTodo(insertId);
-    expect(data.affectedRows).toEqual(1);
-    expect(data).toBeTruthy();
-    const checkId = await db.getTodo(insertId);
-    expect(checkId.length).toEqual(0);
-});
-
-// verify insertId no loger exists
+})
